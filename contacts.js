a28acce36d4499ed767bb6bad68b62085ae12f39
Oleg Palashynskyi <palah@ukr.net>
2022-11-15 09:41:21 +0200

    Update contacts.js

--- contacts.js
+++ contacts.js
@@ -4,12 +4,11 @@ const path = require("path");
 const contactsPath = path.resolve("./db/contacts.json");
 
 async function listContacts() {
-  console.log(123);
   fs.readFile(contactsPath)
     .then((data) => console.log(JSON.parse(data.toString())))
     .catch((err) => console.log(err.message));
-  console.log(321);
 }
+
 // async function listContacts() {
 //   try {
 //     const data = await fs.readFile(contactsPath, "utf8");
@@ -19,9 +18,34 @@ async function listContacts() {
 //   }
 // }
 
-function getContactById(contactId) {
-  // ...твій код
+// listContacts();
+
+async function getContactById(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath, "utf8");
+    const parsedData = JSON.parse(data);
+    const contactById = await parsedData.find(
+      (el) => el.id === contactId.toString()
+    );
+    console.log(contactById);
+  } catch {
+    console.error(err);
+  }
 }
+
+// async function getContactById(contactId) {
+//   fs.readFile(contactsPath)
+//     .then((data) => {
+//       const parsedData = JSON.parse(data.toString());
+//       const contactById = parsedData.find(
+//         (el) => el.id === contactId.toString()
+//       );
+//       return console.log(contactById);
+//     })
+//     .catch((err) => console.log(err.message));
+// }
+
+getContactById(7);
 
 function removeContact(contactId) {
   // ...твій код
